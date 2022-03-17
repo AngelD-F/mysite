0b120abb6fa23e8c2351f3bbaa64e44cb67ba260
AngelD-F <angeldfuenmayor21@gmail.com>
2022-03-16 22:50:28 -0400

    Minor changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import secret
-SECRET_KEY = secret.SECRET_KEY#'##################################'
+from .secret import _SECRET_KEY_
+SECRET_KEY = _SECRET_KEY_#'##################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
