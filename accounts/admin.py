e661bc928d065e4c697c0c59c1a77fdf5ee40d4b
AngelD-F <angeldfuenmayor21@gmail.com>
2022-03-23 20:31:10 -0400

    Adding models to the admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import customer
+from .models import customer, order, product
 
 admin.site.register(customer)
+admin.site.register(product)
+admin.site.register(order)
