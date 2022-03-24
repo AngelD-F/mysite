0bb973a6c5474d5dc925b7c0d0d4070140e12272
AngelD-F <angeldfuenmayor21@gmail.com>
2022-03-23 20:27:46 -0400

    models class for product and order

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
+from telnetlib import STATUS
+from traceback import print_exception
+from unicodedata import category, name
 from django.db import models
 
 # Create your models here.
@@ -10,3 +13,27 @@ class customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class product(models.Model):
+    CATEGORY = (
+        ("Ondoor", "Indoor"),
+        ("Out door", "Out door"),
+    )
+
+    name            = models.CharField(max_length=200, null=True)
+    price           = models.FloatField(null=True)
+    category        = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description     = models.CharField(max_length=200, null=True)
+    date_created    = models.DateTimeField(auto_now_add=True, null=True)
+
+class order(models.Model):
+    STATUS = (
+        ("Pending", "Pending"),
+        ("Out for delivery", "Out for delivery"),
+        ("Delivered", "Delivered"),
+    )
+
+    # customer =
+    # product =
+    date_created    = models.DateTimeField(auto_now_add=True, null=True)
+    status          = models.CharField(max_length=200, null=True, choices=STATUS)
