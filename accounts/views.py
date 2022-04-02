16302cd71074b62ec1ca6dee2c04f7f4ea888062
AngelD-F <angeldfuenmayor21@gmail.com>
2022-04-02 18:43:04 -0400

    updating dasboard template

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import *
+from . import models as mdl
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = mdl.order.objects.all()
+    customers = mdl.customer.objects.all()
+
+    ctx = {'orders': orders, 'customers': customers}
+
+    return render(request, 'accounts/dashboard.html', ctx)
 
 def products(request):
-    products = product.objects.all()
+    products = mdl.product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
