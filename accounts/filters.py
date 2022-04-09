a4d60e231c7a35153fbf468a08f42ad7340b16df
AngelD-F <angeldfuenmayor21@gmail.com>
2022-04-09 15:45:32 -0400

    improving customer filtering

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,9 +1,25 @@
+from django.forms import SelectDateWidget
 import django_filters
+from django_filters import DateFilter
 
 from .models import order
 
 class orderFilter(django_filters.FilterSet):
+    start_date = DateFilter(
+        field_name='date_created',
+        lookup_expr='gte',
+        label='Creado desde',
+        widget=SelectDateWidget()
+    )
+    end_date = DateFilter(
+        field_name='date_created',
+        lookup_expr='lte',
+        label='Creado hasta',
+        widget=SelectDateWidget()
+    )
+
     class Meta:
         model = order
         fields = '__all__'
+        exclude = ['customer', 'date_created']
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
     'django_filters',
+    
+    'accounts',
 ]
 
 MIDDLEWARE = [
